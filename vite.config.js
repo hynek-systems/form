f01c599fbddbacce135ceb7613b3ab84fcfb5c4b
Henrik Söderlind <henriksoderlind@MacBook-Pro-som-tillhor-Henrik-2.local>
2025-09-01 20:21:41 +0200

    Install and configure vite

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from "vite"
+import path from "path"
+
+export default defineConfig({
+    build: {
+        lib: {
+            entry: path.resolve(__dirname, "resources/js/index.js"), // din main JS
+            name: "Hynek/Form",
+            fileName: (format) => `hynek-form.${format}.js`,
+        },
+        outDir: "dist",
+        emptyOutDir: true,
+    },
+})
